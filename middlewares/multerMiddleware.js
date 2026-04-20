a977e463a8faa0fe80c191d7968a57832258f8ae
unknown <amanmuhammad567@gmail.com>
2026-04-20 18:27:50 +0500

    Some Changes

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,17 +1,27 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadPath = path.resolve("./tmp/my-uploads");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './tmp/my-uploads')
+    // create folder if it doesn't exist
+    fs.mkdir(uploadPath, { recursive: true }, (err) => {
+      if (err) return cb(err, uploadPath);
+      cb(null, uploadPath);
+    });
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
-const upload = multer({ storage ,
-    limits: { fileSize: 10 * 1024 * 1024 }
-})
+const upload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
 
-export default upload
+export default upload;
