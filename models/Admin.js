ad1179e23e1404f179a549cfa2b2b17c4d38fbbf
unknown <amanmuhammad567@gmail.com>
2026-04-20 13:27:54 +0500

    Some Changes

--- models/Admin.js
+++ models/Admin.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
 
 const adminSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true }
 }, { timestamps: true },{ collection : 'Admin'})
