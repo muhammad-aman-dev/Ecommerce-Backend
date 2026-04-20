dc5b55f1cc6724410145863d09891601bd332b07
unknown <amanmuhammad567@gmail.com>
2026-04-20 12:59:14 +0500

    Some Changes

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ import User from "../models/Users.js"
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback" // This must match your Google console redirect URI
+    callbackURL: process.env.BACKEND_URL+"/auth/google/callback" // This must match your Google console redirect URI
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
