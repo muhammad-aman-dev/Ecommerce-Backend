b0487766883719ee6c434c3c067e9c50e8d27b84
Muhammad Aman <amanmuhammad567@gmail.com>
2026-09-19 00:57:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ dotenv.config();
 
 const app = express();
 
-console.log("FRONTEND_URL:", process.env.CLIENT_URL);
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
 app.use(cookieParser());
 
 // ---------------- MIDDLEWARE ----------------
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
